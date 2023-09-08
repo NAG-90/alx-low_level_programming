28bc0496eef40a1bae7076e6e01d535c61ece670
NAG-90 <g.nkuefone@gmail.com>
2023-09-07 19:49:59 -0700

    Size of type

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char cType;
+	int iType;
+	long lType;
+	double dType;
+	float fType;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(cType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(iType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(lType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(dType));
+	printf("Size of a flaot: %zu byte(s)\n", sizeof(fType));
+	return (0);
+}
