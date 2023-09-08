d54f3ad271e83ee36560466018dc694a7bcb845e
NAG-90 <g.nkuefone@gmail.com>
2023-09-07 19:17:50 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char output[] = "Programming is like building a multilingual puzzle";
+	char output[] = "\"Programming is like building a multilingual puzzle";
+	
 	puts(output);
 
-	return 0;
+	return (0);
 }
