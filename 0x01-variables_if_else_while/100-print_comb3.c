f24d21a460bbd9d37f58970584b62a64d3b5406e
NAG-90 <g.nkuefone@gmail.com>
2023-09-09 02:57:28 -0700

    Digits combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,17 +12,14 @@ int main(void)
 
 	for (i = 48; i <= 56; i++)
 	{
-		for (j = i + 1; j <= i + 1; j++)
+		for (j = i + 1; j <= 57; j++)
 		{
-			if (i != j)
+			putchar(i);
+			putchar(j);
+			if (i != 56 || j != 57)
 			{
-				putchar(i);
-				putchar(j);
-				if (i != 56 || j != 57)
-				{
-					putchar(44);
-					putchar(32);
-				}
+				putchar(44);
+				putchar(32);
 			}
 		}
 	}
