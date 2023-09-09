37ab1dd1e68893a63754ef9096d95bf0b627bc2d
NAG-90 <g.nkuefone@gmail.com>
2023-09-08 18:46:31 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (int i = 30; i <= 39; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
