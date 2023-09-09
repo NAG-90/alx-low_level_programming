f69a60643ba1673bb3dc68fe692c18dbafe1ff20
NAG-90 <g.nkuefone@gmail.com>
2023-09-08 20:04:18 -0700

    Digits separate by comma

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,16 @@ int main(void)
 {
 	int i = 48;
 
-	putchar(i);
 	while (i < 57)
 	{
-		i++;
-		putchar(44);
-		putchar(32);
 		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
 	}
+	putchar(10);
 	return (0);
 }
