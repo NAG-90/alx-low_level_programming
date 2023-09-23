da473aee8b8a18a05e74115af8da27a1d1ec4431
NAG-90 <g.nkuefone@gmail.com>
2023-09-22 22:50:32 -0700

    Print String in reverse order

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -17,3 +17,20 @@ void print_rev(char *str)
 	}
 	_putchar('\n');
 }
+
+/**
+ * _strlen - calculate the lenght of a string
+ * @s: string to determine lenght
+ *
+ * Return: the lenght of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
