024b66406f73fb3f8bb76e90a9bb07605e692250
NAG-90 <g.nkuefone@gmail.com>
2023-09-24 01:55:13 -0700

    Print half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * puts_half: print half of a given string
+ * @str: string to print
+ *
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int lenght;
+	int i;
+
+	lenght = _strlen(str);
+	if (lenght % 2 == 0)
+		i = lenght / 2;
+	else
+		i = (lenght + 1) / 2;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
+
+/**
+ * _strlen - calculate the lenght of the given string
+ * @s: string to compute
+ *
+ * Return: lenght of the string
+ */
+int void _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
