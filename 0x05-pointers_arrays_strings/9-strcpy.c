0a2dd8dde254fdb8658a14aaa757dd8a6dd8f9a0
NAG-90 <g.nkuefone@gmail.com>
2023-09-25 13:45:51 -0700

    Copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy a string
+ * @dest: a pointer to a dest
+ * @src: a pointer to a source
+ *
+ * Return: a pointer to a dest
+ */
+char *strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (*(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}
